843448667ffb68d5340f7ebacdb577557b758ff8
Andre A. Boechat <boechat@das.ufsc.br>
2012-05-18 18:13:29 -0300

    Funtion to create a hitogram plot with JFreeChart

--- src/image_processing/charts.clj
+++ src/image_processing/charts.clj
@@ -1,57 +1,74 @@
 (ns image-processing.charts
     (:import
-      [org.jfree.chart ChartFactory ChartUtilities ChartFrame ChartTheme StandardChartTheme]
+      [java.awt Color]
+      [org.jfree.chart  ChartFactory
+                        ChartUtilities
+                        ChartFrame
+                        ChartTheme
+                        StandardChartTheme]
       [org.jfree.data.statistics    HistogramDataset
                                     HistogramType
-                                    DefaultBoxAndWhiskerCategoryDataset])
+                                    DefaultBoxAndWhiskerCategoryDataset]
+      [org.jfree.data.xy XYSeries XYSeriesCollection])
     (:use
       [seesaw core make-widget]))
 
 
-;(defn histogram*
-;  ([x & options]
-;    (let [opts (if options (apply assoc {} options) {})
-;          data (:data opts)
-;          _x (if (coll? x) (to-list x) ($ x data))
-;          nbins (or (:nbins opts) 10)
-;          theme (or (:theme opts) :default)
-;          density? (true? (:density opts))
-;          title (or (:title opts) "")
-;          x-lab (or (:x-label opts) (str 'x))
-;          y-lab (or (:y-label opts)
-;                     (if density? "Density" "Frequency"))
-;          series-lab (or (:series-label opts) (str 'x))
-;          legend? (true? (:legend opts))
-;          dataset (HistogramDataset.)]
-;      (do
-;        (.addSeries dataset series-lab (double-array _x) nbins)
-;        (when density? (.setType dataset org.jfree.data.statistics.HistogramType/SCALE_AREA_TO_1))
-;        (let [chart (-> (org.jfree.chart.ChartFactory/createHistogram
-;                          title
-;                          x-lab
-;                          y-lab
-;                          dataset
-;                          org.jfree.chart.plot.PlotOrientation/VERTICAL
-;                          legend?		; no legend
-;                          true			; tooltips
-;                          false)
-;                        (set-theme theme))]
-;          chart)))))
+; TODO: option to rotate the histogram for horizontal histograms.
+(defn histogram-chart
+  "Generates a JFree histogram object for a collection X of frequencies.
+   OPTS is a map of options:
+   :title
+   :x-label
+   :y-label
+   :series-label
+   :legend?
+
+   EXAMPLE:
+   (view (histogram-chart [1 5 3 10 7])
+   
+   References:
+   http://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/JFreeChart.html
+   https://github.com/liebke/incanter/blob/master/modules/incanter-charts/src/incanter/charts.clj "
+  #^{:arglists [[x] [x opts]]}
+  ([x] (histogram-chart x nil))
+  ([x opts]
+   (let [title (or (:title opts) "Histogram")
+         x-lab (or (:x-label opts) (str 'x))
+         y-lab (or (:y-label opts) "Frequency")
+         series-lab (or (:series-label opts) (str 'x))
+         legend? (true? (:legend opts))
+         background-img (or (:bg-img opts) nil)
+         data-series (XYSeries. series-lab)
+         dataset (XYSeriesCollection.)]
+     (dorun (map #(.add data-series %1 %2) (range 1 (inc (count x))) x)) 
+     (.addSeries dataset data-series)
+     (let [chart (org.jfree.chart.ChartFactory/createHistogram
+                   title
+                   x-lab
+                   y-lab
+                   dataset
+                   org.jfree.chart.plot.PlotOrientation/VERTICAL
+                   legend?		; no legend
+                   true			; tooltips
+                   false)] 
+       (.setBackgroundImage chart background-img) ; does not work
+       (.setBackgroundPaint chart (Color. 0 0 0 0)) ; does not work
+       chart))))
 
 
 (defmulti view (fn [arg] (type arg)))
 
 (defmethod view org.jfree.chart.JFreeChart
-  ([chart & options]
-    (let [opts (when options (apply assoc {} options))
-          window-title (or (:window-title opts) "Incanter Plot")
-          width (or (:width opts) 500)
-          height (or (:height opts) 400)
-          frame (ChartFrame. window-title chart)]
-      (doto frame
-        (.setSize width height)
-        (.setVisible true))
-      frame)))
+  [chart]
+  (let [window-title "JFree Chart"
+        width 500
+        height 400
+        frame (ChartFrame. window-title chart)]
+    (doto frame
+          (.setSize width height)
+          (.setVisible true))
+    frame))
 
 (defmethod view java.awt.image.BufferedImage
   [buff-img]
