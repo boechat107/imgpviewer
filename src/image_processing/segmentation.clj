af385bbd5f857a4d2289844d2768c2698cfb0f9a
andre.boechat <andre.boechat@2c24e366-a514-11e1-afb8-b081dd72f066>
2012-05-24 21:46:20 +0000

    Horizontal crop and some improvements.

--- src/image_processing/segmentation.clj
+++ src/image_processing/segmentation.clj
@@ -1,20 +1,44 @@
 (ns image-processing.segmentation
     (:use
-      [image-processing.core :only (vertical-histogram)]
+      [image-processing.core :only (vertical-histogram horizontal-histogram)]
       [image-processing.image :only (get-img-height get-subimage)]))
 
 
+(defn- get-limits-histogram-autocrop
+  "Doc"
+  #^{:arglists [[img] [img options]]}
+  [img histogram-func options]
+  (let [threshold (or (:threshold options) 1)
+        hist (histogram-func img)
+        vec (partition 2 (interleave (range (count hist)) hist)) 
+        l1 (first (first (filter #(> (second %) threshold) vec)))
+        l2 (first (first (filter #(> (second %) threshold) (reverse vec))))]
+    (if (and (not (nil? l1)) (>= l1 l2))
+      [nil nil]
+      [l1 l2])))
+
+
 (defn vertical-histogram-autocrop
-  "Options:
+  "Removes blank columns from the left and/or right of the image.
+   If all image is croped, nil is returned.
+   Options:
       :threshold (default 1)"
   #^{:arglists [[img] [img options]]}
   ([img] (vertical-histogram-autocrop img {}))
   ([img options]
-   (let [threshold (or (:threshold options) 1)
-         v-hist (vertical-histogram img)
-         vec (partition 2 (interleave (range (count v-hist)) v-hist)) 
-         x1 (first (first (filter #(> (second %) threshold) vec)))
-         x2 (first (first (filter #(> (second %) threshold) (reverse vec))))]
+   (let [[x1 x2] (get-limits-histogram-autocrop img vertical-histogram options)]
      (if (or x1 x2)
-       (get-subimage img x1 0 (- x2 x1) (get-img-height img))
-       img))))
+       (get-subimage img x1 0 (- x2 x1) (get-img-height img))))))
+
+
+(defn horizontal-histogram-autocrop
+  "Removes blank lines from the top and/or the bottom of the image.
+   If all image is croped, nil is returned.
+   Options:
+      :threshold (default 1)"
+  #^{:arglists [[img] [img options]]}
+  ([img] (horizontal-histogram-autocrop img {}))
+  ([img options]
+   (let [[y1 y2] (get-limits-histogram-autocrop img horizontal-histogram options)]
+     (if (or y1 y2)
+       (get-subimage img 0 y1 (:width img) (- y2 y1))))))
