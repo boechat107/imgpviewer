78678474e80668f213f9c697f8a9e30975e83a35
Andre A. Boechat <boechat@das.ufsc.br>
2012-05-28 16:21:43 -0300

    Change of the Image structure and functions updates

---
+++ src/image_processing/transformations.clj
@@ -0,0 +1,14 @@
+(ns image-processing.transformations
+    (:use
+      [image-processing.basic-math :only (mean)]
+      [image-processing.image])
+    (:import
+      [image_processing.image Image]))
+
+
+
+
+
+
+
+
