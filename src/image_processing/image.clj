153d4f4a8142496334d7c2b4c69fe89b800134ce
Andre A. Boechat <boechat107@gmail.com>
2012-05-30 17:25:53 -0300

    Erode transformation, convolution function and lazy sequences
    It's assumed that the pixels of a Image compose a lazy sequence
    instead of a vector.

--- src/image_processing/image.clj
+++ src/image_processing/image.clj
@@ -1,47 +1,77 @@
 (ns image-processing.image)
 
-(defprotocol Points
-  "This abstracts the ways to access points."
-  (get-subset [this x y w h] "Returns the subset defined by a rectangular region.")
-  (get-point [this x y] "Returns the content of a specific point."))
+;;; image structure
+;[ [{:x :y | :a 0-255 :r 0-255 :g 0-255 :b 0-255 | :gray 0-255 | :bw 0-1}, ...], WIDTH]
 
-(defrecord Image [points width]
-  Points
-  (get-subset [img x y w h]
-              (reduce #(apply conj %1 (subvec points %2 (+ %2 w)))
-                      []
-                      (range
-                        (+ x (* y width))
-                        (+ x (* (+ y h) width))
-                        width)))
-  (get-point [img x y]
-             (when (< x width)
-               (get points (+ x (* y width))))))
+(defrecord Image [pixels width])
 
-(defn get-img-height
-  "Doc"
-  #^{:arglists [img]}
+(defn get-image-type
+  "Returns the type of a image structure:
+      :argb, :bw or :gray
+      OR nil if it is not a image."
   [img]
-  (/ (count (:points img)) (:width img)))
+  (let [pix (first (:pixels img))]
+    (cond
+      (:bw pix) :bw
+      (:gray pix) :gray
+      (and (:r pix) (:g pix) (:b pix)) :argb
+      :else nil)))
 
 
-(defn get-subimage
+(defn get-pixel
+  "Gets the pixel [x y] of a image structure."
+  [img x y]
+  (nth (:pixels img) (+ x (* y (:width img)))))
+
+
+(defn get-height
   "Doc"
-  #^{:arglists [img x y w h]}
-   [img x y w h]
-   (Image.
-     (reduce #(apply conj %1 (subvec (:points img) %2 (+ %2 w)))
-             []
-             (range
-               (+ x (* y (:width img)))
-               (+ x (* (+ y h) (:width img)))
-               (:width img)))
-     w))
+  [img]
+  (/ (count (:pixels img)) (:width img)))
 
 
-(defn image?
-  "Returns true if ARG is a Image, false otherwise."
-  #^{:arglists [arg]}
-  [arg]
-  (instance? image_processing.image.Image arg))
+(defn get-pixels-of-line
+  "Gets a lazy sequence with the pixels of a line."
+  [img line]
+  (nth (partition (:width img) (:pixels img)) 
+       line))
+
+
+(defn get-pixels-of-column
+  "Gets a lazy sequence with the pixels of a column."
+  [img column]
+  (nth (partition (get-height img)
+                  (apply interleave (partition (:width img)
+                                               (:pixels img))))
+       column))
+
+
+(defn get-image-abs-coords
+  "Gets a Image with the absolute coordinates setted according to the order of 
+   the pixels."
+  [pixels width]
+  (Image. 
+    (map #(assoc %1 :x (first %2) :y (second %2))
+         pixels
+         (for [y (range (/ (count pixels) width)), x (range width)] [x y]))
+    width))
+
+
+
+;todo: Iterate over the lazy sequence and returns a lazy sequence.
+; multimethod to handle with a LazySeq and with a Vector (for performance purpose).
+(defn get-subimage
+  "Gets a subimage determined by a square with W width, H height and starting points X and
+   Y."
+  [img x y w h]
+  ;; A vector structure is needed to use subvec.
+  (let [pixels (if (vector? (:pixels img)) (:pixels img) (vec (:pixels img)))
+        new-pixels (reduce #(apply conj %1 (subvec pixels %2 (+ %2 w)))
+                           []
+                           (range
+                             (+ x (* y (:width img)))
+                             (+ x (* (+ y h) (:width img)))
+                             (:width img)))]
+    (get-image-abs-coords new-pixels w)))
+
 
--- src/image_processing/transformations.clj
+++ src/image_processing/transformations.clj
@@ -1,14 +1,83 @@
 (ns image-processing.transformations
     (:use
       [image-processing.basic-math :only (mean)]
-      [image-processing.image])
+      [image-processing.image :only (get-pixel
+                                     get-height
+                                     get-image-type
+                                     get-pixels-of-column
+                                     get-pixels-of-line
+                                     get-image-abs-coords)])
     (:import
       [image_processing.image Image]))
 
 
+(defn- convolution
+  "General routine to convolute a mask over a Image."
+  [img mask op-func val-func]
+  (let [old-pixels (:pixels img)
+        pixels (if (vector? old-pixels) old-pixels (vec old-pixels))
+        height (get-height img)
+        img-vec (Image. pixels (:width img))
+        max-bound-func (fn [num] (if (>= num 0.5) 1 0))
+        vals (map (fn [center & more]
+                      (let [[xc yc] center] 
+                        (->> (reduce op-func ;specific color type code.
+                                     0.0
+                                     (partition 2
+                                                (interleave 
+                                                  mask
+                                                  (map #(get-pixel img-vec (% 0) (% 1))
+                                                       (for [yn (range (dec yc) (+ 2 yc))
+                                                             xn (range (dec xc) (+ 2 xc))]
+                                                            [xn yn])))))
+                             (val-func)))) ; specific color type code.
+                  ;; Image coordinates without border.
+                  (for [y (range 1 (dec height))
+                        x (range 1 (dec (:width img)))]
+                       [x y]))
+        ;;; Joins the new pixel values with the original border of the image.
+        ;; Gets the first and last lines, both without the fisrt and last element because
+        ;; they are elements of the first and last columns too.
+        rem-first-last (fn [seq] (->> seq (rest) (drop-last)))
+        first-line (rem-first-last (get-pixels-of-line img-vec 0))
+        last-line (rem-first-last (get-pixels-of-line img-vec (dec height)))
+        vals-lines (partition (- (:width img) 2) vals)
+        ;; Concatenates the lines, new ones and the borders of the original image.
+        with-lines (concat [first-line] vals-lines [last-line])
+        first-col (get-pixels-of-column img-vec 0)
+        last-col (get-pixels-of-column img-vec (dec (:width img-vec)))
+        vals-cols (partition (get-height img) (apply interleave with-lines))
+        ;; Concatenates the columns.
+        with-cols (concat [first-col] vals-cols [last-col])]
+    (get-image-abs-coords (apply interleave with-cols) (:width img))))
 
 
+;TODO: optimize performance
+(defmulti convolve
+          "Convolute a 3x3 mask over a Image. The mask must be a simple vector with
+           9 real values."
+          (fn [img mask] 
+              (get-image-type img)))
 
 
+(defmethod convolve :bw
+  [img mask]
+  (let [op-func (fn [cum mask-bw]
+                    (+ cum (* (first mask-bw) (:bw (second mask-bw)))))
+        val-func (fn [val]
+                     (if (>= val 0.5) {:bw 1} {:bw 0}))]
+    (convolution img mask op-func val-func)))
 
+
+(defn erode
+  "Erodes a Image, a basic operation in the area of the mathematical morphology.
+   http://homepages.inf.ed.ac.uk/rbf/HIPR2/erode.htm
+
+   The corner and edge values of the mask can be specified. The default values are 0.2."
+   ([img] (erode img 0.2 0.2))
+   ([img corner edge]
+    (let [mask [corner  edge    corner
+                edge    1.0     edge
+                corner  edge    corner]]
+      (convolve img mask))))
 
