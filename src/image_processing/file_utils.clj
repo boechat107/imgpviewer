bafdd0f2c77d53c227c23547b613dd97d2abfba4
vinicius.rocha <vinicius.rocha@2c24e366-a514-11e1-afb8-b081dd72f066>
2012-06-14 15:17:33 +0000

    added grayscale->pixel.clj and some file utils for managing images

---
+++ src/image_processing/file_utils.clj
@@ -0,0 +1,41 @@
+(ns image-processing.file-utils
+  (:import (java.io File)))
+
+(defn File? [file]
+  "Test if object is of type java.io.File"
+  #^{:arglist [file]}
+  (= (class file) java.io.File))
+
+(defn file-name
+  "Given a File, returns its name stripping the extension"
+  #^{:arglist [file]}
+  ([file]
+      {:pre [(File? file)]}
+      (let [filename (.getName file)
+            ext-pos (.lastIndexOf filename ".")]
+        (if (> ext-pos 0)
+          (subs filename 0 ext-pos)
+          filename))))
+
+(defn file-ext
+  "Given a File, returns its extension (by default 4 chars from
+  right to left."
+  #^{:arglist [[file] [file n]]}
+  ([file]
+      {:pre [(File? file)]}
+      (file-ext file 4))
+
+  ([file n]
+      {:pre [(File? file)]}
+      (let [filename (.getName file)]
+        (if (-> filename count (> n))
+          (subs filename (- (count filename) n) (count filename))))))
+
+(defn file-img? [file]
+  "Test if file is an image:
+      -isFile?
+      -extension (jpg png bmp gif)"
+  #^{:arglist [file]}
+  (let [img-exts #{".jpg" ".png" ".bmp" ".gif"}]
+    (if (.isFile file)
+      (contains? img-exts (file-ext file)))))
--- src/image_processing/pixel.clj
+++ src/image_processing/pixel.clj
@@ -1,10 +1,5 @@
 (ns image-processing.pixel)
 
-(defn neighbour-hv? [pixel1 pixel2]
-  (let [delta [(- (:x pixel1) (:x pixel2)) (- (:y pixel1) (:y pixel2))]]
-    (contains? #{[-1 0] [1 0] [0 1] [0 -1]} delta)
-    ))
-
-(defn neighbour-hvd? [pixel1 pixel2]
-  (let [delta [(- (:x pixel1) (:x pixel2)) (- (:y pixel1) (:y pixel2))]]
-    (contains? #{[-1 0] [1 0] [0 1] [0 -1] [-1 -1] [-1 1] [1 -1] [1 1]} delta)))
+(defn grayscale-value [pixel]
+  (/ (apply min ((juxt :r :g :b) pixel))
+     255))
