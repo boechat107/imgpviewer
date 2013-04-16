947a40c674b85340072aa667af4def5fbe053673
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-04-16 13:11:05 -0300

    Convolution operation; erode function

--- src/image_processing/processing.clj
+++ src/image_processing/processing.clj
@@ -29,3 +29,43 @@
     (ipc/make-image (conj (repeat 3 gray-ch)
                           (ic/matrix 255 (ipc/nrows img) (ipc/ncols img)))
                     :argb)))
+
+(defn grid-apply
+  "Returns a sequence resulting from the application of the function f to each 
+  value of the grid built with the rectangle x-min, x-max, y-min, y-max."
+  [f x-min x-max y-min y-max]
+  (for [y (range y-min y-max), x (range x-min x-max)]
+    (f x y)))
+
+(defn convolve
+  [channels mask]
+  ;; todo: speed up with discrete Fourier transform.
+  ;; variable mask size.
+  (let [nr (ic/nrow (first channels))
+        nc (ic/ncol (first channels))
+        real-xy (fn [rc c m] 
+                  ;; Returns c if it is between the boundaries of the image. 
+                  (if (or (neg? c) (== c m)) rc c))
+        kernel (fn [mat x y] 
+                 (->> (for [ky (range (dec y) (+ 2 y)),
+                            kx (range (dec x) (+ 2 x))]
+                        (ic/$ (real-xy y ky nr) (real-xy x kx nc) mat))
+                      (map #(* %1 %2) mask)
+                      ic/sum))]
+    (->> (map #(grid-apply (partial kernel %)
+                           0 nc 0 nr)
+              channels) 
+      (map #(ic/matrix % nc)))))
+
+(defn erode
+  "Erodes a Image, a basic operation in the area of the mathematical morphology.
+   http://homepages.inf.ed.ac.uk/rbf/HIPR2/erode.htm
+   The corner and edge values of the mask can be specified. The default values are 0.2."
+   ([img] (erode img 0.2 0.2))
+   ([img corner edge]
+    {:pre [(ipc/gray-type? img)]}
+    (let [mask [corner  edge    corner
+                edge    1.0     edge
+                corner  edge    corner]]
+      (-> (convolve (:channels img) mask)
+          (ipc/make-image (:type img))))))
