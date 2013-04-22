9779b82ade16d48361e2ca978d41087391ac3191
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-04-22 16:13:59 -0300

    Generic function to multiply vectors

--- src/image_processing/utils.clj
+++ src/image_processing/utils.clj
@@ -4,7 +4,6 @@
         [clojure.set]
         [clojure.pprint]))
 
-
 (defn print-methods-table [obj]
   "pprint all methods, parameter, and return types of a object,
 sorted by method name."
@@ -15,3 +14,24 @@ sorted by method name."
     ;; (println (format "Bases --> %s%nAbove table for class --> %s"
     ;;                  (:bases reflection) obj))
     ))
+
+(defn mult-vec
+  "Applies a element-by-element multiplication among vectors. 
+  EX.: (mult-vec [1 2 3] 2)
+       (mult-vec 2 [2 3 4] [2 2 2] 1)"
+  [v & vs]
+  (let [step (fn [v a]
+               (cond 
+                 (and (sequential? v) (sequential? a))
+                 (map * v a)
+                 ;
+                 (sequential? v)
+                 (map #(* a %) v)
+                 ;
+                 (sequential? a)
+                 (map #(* v %) a)
+                 ;
+                 :else (* v a)))]
+    (if (seq vs)
+      (recur (step v (first vs)) (rest vs))
+      v)))
