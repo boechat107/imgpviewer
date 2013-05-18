14e715d784617ff28853301a951c28c437a5f7f8
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-05-18 12:57:55 -0300

    Benchmarking with imagez
    * This lib beats for simple tasks, but it is very much slower for more
    complex routines.

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :description "An image processing library using Clojure data structures."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [incanter/incanter-core "1.5.0-SNAPSHOT"]
+                 [net.mikera/imagez "0.0.2"]
                  [seesaw "1.4.3"]]
+  :resource-paths ["test/"]
+  :global-vars {*warn-on-reflection* true, *assert* false}
   :jar-exclusions [#"(?:^|/).svn/"])
--- test/image_processing/test/processing.clj
+++ test/image_processing/test/processing.clj
@@ -2,7 +2,13 @@
   (:require 
     [image-processing.core-new :as ipc]
     [image-processing.processing :as pr]
-    [image-processing.helpers :as ih])
+    [image-processing.helpers :as ih]
+    [mikera.image.core :as mc]
+    [mikera.image.filters :as mf]
+    )
+  (:import 
+    [java.awt.image BufferedImage]
+    )
   )
 
 (defn time-test 
@@ -14,3 +20,15 @@
     nil
     ;(time (ih/view img gray bw er))
     ))
+
+(defn imagez-test
+  []
+  (let [img (time (mc/load-image "ds.jpg"))
+        gray (time (mc/filter-image (mf/grayscale) img))
+        bw (time (mc/filter-image (mf/quantize 0) gray))
+        blur (time (mc/filter-image (mf/blur) img))
+        ]
+    nil
+    (time (ih/view bw))
+    )
+  )
