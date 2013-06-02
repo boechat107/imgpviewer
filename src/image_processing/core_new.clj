f7095136b16becac88bc17312c1040be59a0ca6c
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-06-02 12:21:24 -0300

    Change of the multdimensional array for a vector of 1D arrays
    * It is little faster, but not enough

--- src/image_processing/core_new.clj
+++ src/image_processing/core_new.clj
@@ -24,7 +24,8 @@
 
 (defn mat?
   [m]
-  (= (type m) (Class/forName "[[[I")))
+  (and (sequential? m)
+       (every? #(= (type %) (Class/forName "[I")) m)))
 
 (defn color-type?
   [img]
@@ -55,27 +56,27 @@
 
 (defn new-channel-matrix 
   "Returns a matrix used to represent a color channel data."
-  [nrows ncols dim] 
-  (make-array Integer/TYPE nrows ncols dim))
+  [nrows ncols] 
+  (make-array Integer/TYPE (* nrows ncols)))
 
 (defn make-image
   "Returns an instance of Image for a given image data, its number of columns of
   pixels and the color space of the image. 
   The image data is stored as different channels, each one as a clojure.matrix, and
   the value of each pixel a double value."
-  ([data-chs type]
-   {:pre [(valid-type? type) (mat? data-chs)]}
-   (Image. data-chs
-           type
-           (alength #^objects data-chs) 
-           (alength #^objects (aget #^objects data-chs 0)))))
+  ([data-chs nrows ncols type]
+   {:pre [(valid-type? type) (mat? data-chs)
+          (every? #(== (alength ^ints %) (* nrows ncols)) data-chs)]}
+   (Image. (if (vector? data-chs) data-chs (vec data-chs))
+           type nrows ncols)))
 
 (defn new-image
   "Returns an empty image with the given dimension and color type."
   [nrows ncols type]
   {:pre [(contains? color-dimensions type)]}
-  (-> (new-channel-matrix nrows ncols (type color-dimensions))
-      (make-image type)))
+  (-> (type color-dimensions)
+      (repeatedly #(new-channel-matrix nrows ncols))
+      (make-image nrows ncols type)))
 
 ;(defn copy-image
 ;  "Returns a copy of a given image."
@@ -83,13 +84,19 @@
 ;  (-> (map #(ut/mult-aclone %) (:mat img)) 
 ;      (make-image (:type img))))
 
+(defn xy-to-pos 
+  "Converts a index [x, y] to a index for a continuous array."
+  [x y ncols]
+  (-> (* y ncols) (+ x)))
+
 (defn get-pixel
   "Returns the value of the pixel [x, y]. If no channel is specified, a vector is
   returned; otherwise, a scalar is returned."
   ([img x y]
-   (mapv #(ut/mult-aget ints (:mat img) y x %) (range (dimension img))))
+   (mapv #(ut/mult-aget ints % (+ (* y (ncols img)) x)) 
+         (:mat img)))
   ([img x y ch]
-   (ut/mult-aget ints (:mat img) y x ch)))
+   (ut/mult-aget ints ((:mat img) ch) (+ (* y (ncols img)) x))))
 
 (defn get-neighbour
   "Returns the value of one of the pixels of a squared area around [x,y].
@@ -112,12 +119,14 @@
 
 (defn set-pixel!
   "Sets the value of the [x, y] pixel."
-  ([img x y vals]
-   (dorun 
-     (map-indexed #(ut/mult-aset ints (:mat img) y x %1 %2)
-                  vals)))
+;  ([img x y vals]
+;   (dorun 
+;     (map-indexed #(ut/mult-aset ints (:mat img) y x %1 %2)
+;                  vals)))
   ([img x y ch val]
-   (ut/mult-aset ints (:mat img) y x ch val)))
+   (ut/mult-aset ints ((:mat img) ch)
+                 (+ x (* y (ncols img))) 
+                 val)))
 
 (defn grid-apply
   "Returns a lazy sequence resulting from the application of the function f to each 
