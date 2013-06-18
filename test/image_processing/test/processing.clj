8b66c669a815f588cd124a2661dc1249ef05ccfa
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-06-17 23:42:32 -0300

    Manually generated image for testing

--- test/image_processing/test/processing.clj
+++ test/image_processing/test/processing.clj
@@ -1,15 +1,35 @@
 (ns image-processing.test.processing 
+  (:use 
+    [clojure.test]
+    [image-processing.core-new]
+    )
   (:require 
-    [image-processing.core-new :as ipc]
     [image-processing.processing :as pr]
     [image-processing.helpers :as ih])
   )
 
+(defn img-test
+  "A manually created image."
+  (let [img (c/new-image 4 4 :rgb)
+        [rch gch bch] (c/get-channel img)]
+    (c/set-pixel! rch 0 255)
+    (c/set-pixel! gch 0 255)
+    (c/set-pixel! bch 0 255)
+    (c/set-pixel! gch 5 255)
+    (c/set-pixel! bch 5 255)
+    (c/set-pixel! rch 10 255)
+    (c/set-pixel! bch 10 255)
+    (c/set-pixel! rch 15 255)
+    (c/set-pixel! gch 15 255)))
+
 (defn time-test 
   []
-  (let [img (time (ih/load-file-image "test/cnh.png"))
+  (let [img (time (ih/load-file-image "test/ds.jpg"))
         gray (time (pr/rgb-to-gray img)),
-;        bw (time (pr/binarize gray 100)),
-;        er (time (pr/erode bw))
+        bw (time (pr/binarize gray 100)),
+        er (time (pr/erode bw))
+        blur (time (pr/big-blur img))
+        sblur (time (pr/small-blur img))
         ]
-    (time (ih/view img gray))))
+    (time (ih/view blur sblur img))
+    nil))
