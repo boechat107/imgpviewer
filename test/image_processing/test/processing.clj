ff434b9e4b55172b3710605b824e554780696933
Andre Ambrosio Boechat <boechat107@gmail.com>
2013-06-19 14:46:06 -0300

    Test to check the pixels' value

--- test/image_processing/test/processing.clj
+++ test/image_processing/test/processing.clj
@@ -11,14 +11,33 @@
 (def img-test
   "A manually created image."
   (let [img (new-image 4 4 :rgb)
-        [rch gch bch] (get-channel img)]
+        [rch gch bch] (get-channel img)
+        fname "test/img_test.png"]
     (set-pixel! rch 0 255)
     (set-pixel! gch 0 255)
     (set-pixel! bch 0 255)
     (set-pixel! rch 5 255)
     (set-pixel! gch 10 255)
     (set-pixel! bch 15 255)
-    img))
+    (ih/save-to-file! img fname)
+    (ih/load-file-image fname)))
+
+(deftest pixel-vals
+  (let [rch (get-channel img-test 0)
+        gch (get-channel img-test 1)
+        bch (get-channel img-test 2)]
+    (is (== 255 (get-pixel rch 0)))
+    (is (== 255 (get-pixel gch 0)))
+    (is (== 255 (get-pixel bch 0)))
+    (is (== 255 (get-pixel rch 5)))
+    (is (== 0 (get-pixel gch 5)))
+    (is (== 0 (get-pixel bch 5)))
+    (is (== 0 (get-pixel rch 10)))
+    (is (== 255 (get-pixel gch 10)))
+    (is (== 0 (get-pixel bch 10)))
+    (is (== 0 (get-pixel rch 15)))
+    (is (== 0 (get-pixel gch 15)))
+    (is (== 255 (get-pixel bch 15)))))
 
 (defn time-test 
   []
